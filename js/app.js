88b29b7b367cb86253ec2684ec3fea6901eaffe3
Umesh <umeshitukade1999@gmail.com>
2021-04-14 12:30:03 +0530

    About & Skills section completed

--- js/app.js
+++ js/app.js
@@ -3,5 +3,23 @@ $(document).ready(function () {
     $('#profile-ripple').ripples({
         resolution: 512,
         dropRadius:5
+    })
+    
+    // progressbar percentage
+    const bars = document.querySelectorAll('.progress-bar');
+console.log(bars)
+
+bars.forEach(function (bar) {
+    let percentage= bar.dataset.percent;
+//         // console.log(per)
+    let tooltip = bar.children[0];
+    tooltip.innerText = percentage + '%';
+    bar.style.width=percentage+'%'
+    // console.log(tooltip)
+    
+
+
 })
+  
 });
+
