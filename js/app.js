45004bbbe5c6bdde7d8532c213c50c2855530603
Umesh <umeshitukade1999@gmail.com>
2021-04-15 20:12:56 +0530

    All Completed with responsiveness

--- js/app.js
+++ js/app.js
@@ -21,56 +21,113 @@ bars.forEach(function (bar) {
 
 })
     
-    // Counter Animation
-    const counters = document.querySelectorAll('.counter');
-    // console.log(counters)
+    // // Counter Animation
+    // const counters = document.querySelectorAll('.counter');
+    // // console.log(counters)
 
-    function runCounter() {
-        counters.forEach(counter => {
-            counter.innerText = 0;                                           //initial count
+    // function runCounter() {
+    //     counters.forEach(counter => {
+    //         counter.innerText = 0;                                           //initial count
             
 
 
-            let target = +counter.dataset.count;                                 //final count
-            let step=target/10                                                               
-            // console.log(target)
+    //         let target = +counter.dataset.count;                                 //final count
+    //         let step=target/10                                                               
+    //         // console.log(target)
 
-            let countIt = function () {                                           //recursive function count the num
-                let displayCount=+counter.innerText                             //initial value of count stored in displaycount
-                    if (displayCount < target) {                                //check wheather displaycount is less than target or not & loop agian
-                        counter.innerText = Math.ceil(displayCount +step);       //value  incrented after ever one loop
-                    console.log(displayCount)
-                    // countIt();                                                //this will not give time to browser for incrementation because browser is
-                                                                                  //not getting enough time to increment so by using settimeout it will icrement one by one
-                    setTimeout(countIt, 60);                         
-                }
-                else {
-                    counter.innerText = target;                                       //else final value 
-                }
-            }
-            countIt();
-        })
+    //         let countIt = function () {                                           //recursive function count the num
+    //             let displayCount=+counter.innerText                             //initial value of count stored in displaycount
+    //                 if (displayCount < target) {                                //check wheather displaycount is less than target or not & loop agian
+    //                     counter.innerText = Math.ceil(displayCount +step);       //value  incrented after ever one loop
+    //                 console.log(displayCount)
+    //                 // countIt();                                                //this will not give time to browser for incrementation because browser is
+    //                                                                               //not getting enough time to increment so by using settimeout it will icrement one by one
+    //                 setTimeout(countIt, 60);                         
+    //             }
+    //             else {
+    //                 counter.innerText = target;                                       //else final value 
+    //             }
+    //         }
+    //         countIt();
+    //     })
       
         
-    }
+    // }
 
-    let counterSection = document.querySelector('counter-wrapper');
-    let options = {
-        rootMargin: '0px 0px -100px 0px'
-    }
+    // let counterSection = document.querySelector('counter-wrapper');
+    // let options = {
+    //     rootMargin: '0px 0px -100px 0px'
+    // }
 
 
 
-    const sectionObserver = new IntersectionObserver(function (entry,observe) {           //this method is used to observe a specific part of section
+    // const sectionObserver = new IntersectionObserver(function (entry,observe) {           //this method is used to observe a specific part of section
         
-        if (entry[0].isIntersecting) {
-            console.log('intersecting')
-            runCounter();
+    //     if (entry[0].isIntersecting) {
+    //         console.log('intersecting')
+    //         runCounter();
 
-        }
-    }, options);
+    //     }
+    // }, options);
     
 
-    sectionObserver.observe(counterSection);
+    // sectionObserver.observe(counterSection);
+
+
+
+    // Image Filter
+    // using jquery as we are using plugin /*****isotope.js****** */
+
+    // var $wrapper = $('.portfolio-wrapper');
+    // // console.log(wrapper)
+
+    // // /initialising plugin
+    // $wrapper.isotope({
+    //     filter: '*',                                //fetching from html all 
+    //     layoutMode: 'masnory',
+    //     animationOptions: {
+    //         durations: 750,
+    //         easing: 'linear'
+    //     }
+    
+    // });
+
+    // let links = document.querySelectorAll('.tabs a');       //fetching all links
+    // // console.log(links)
+    // links.forEach(link => {
+    //     link.addEventListener('click', function (e) {
+    //         e.preventDefault();
+    //         console.log('somethinf')
+    //     })
+    // })
+// gallery
+    
+    
+    
+    
+    
+    
+    
+
+
+    
+
+    
+    
+    
+    
+    
+    // gallery  end
+
+    // slick slider
+
+    $('.slider').slick({
+        arrows: false,
+        autoplay: true
+
+    });
+
+
+
 });
 
